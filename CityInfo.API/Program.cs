d1a6496a2736c23431c46ecb997004f0faec6a49
Cosmin <mariancosmin30@yahoo.com>
2023-02-11 21:32:33 +0200

    Use Serilog for logging

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,8 +1,15 @@
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger(); 
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
+//builder.Logging.ClearProviders();
+//builder.Logging.AddConsole();
 
 // Add services to the container.
 
