1aaeb4e88dda38a4cb909b1c8f4052fc02b1c618
Cosmin <mariancosmin30@yahoo.com>
2023-02-12 13:52:47 +0200

    Created initial migration and installed SqlLite and SqlServer toolbox

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -7,5 +7,16 @@ namespace CityInfo.API.DbContexts
     {
         public DbSet<City> Cities { get; set; }
         public DbSet<PointOfInterest> PointsOfInterest { get; set; }
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionString");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
     }
 }
