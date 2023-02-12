90d82c12e98f694dd2a2c9ce6bf0b814c4cf7013
Cosmin <mariancosmin30@yahoo.com>
2023-02-12 12:52:28 +0200

    Configure Connection string on DbContext

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -2,6 +2,7 @@ using CityInfo.API;
 using CityInfo.API.DbContexts;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -38,7 +39,7 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 
 builder.Services.AddSingleton<CitiesDataStore>();
 
-builder.Services.AddDbContext<CityInfoContext>();
+builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite("Data Source=CityInfo.db"));
 
 var app = builder.Build();
 
